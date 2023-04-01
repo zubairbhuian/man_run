9d07b31de6f7fead3770f30a6c5373f5e1e51838
Zubair <74538410+zubairbhuian@users.noreply.github.com>
2023-04-02 00:04:47 +0600

    base

---
+++ lib/game/glob_game.dart
@@ -0,0 +1,11 @@
+import 'dart:async';
+
+import 'package:flame/game.dart';
+
+class GlobGame extends FlameGame{
+  @override
+  FutureOr<void> onLoad() {
+    // TODO: implement onLoad
+    return super.onLoad();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,8 @@
-import 'dart:math' as math;
-
-
-import 'package:flame/components.dart';
-import 'package:flame/experimental.dart';
 import 'package:flame/game.dart';
-import 'package:flame/palette.dart';
 import 'package:flutter/material.dart';
+import 'package:man_run/game/glob_game.dart';
 
 void main() {
-  final game = MyGame();
-  runApp(GameWidget(game: game));
-}
-
-class MyGame extends FlameGame  {
-
-}
-
-class Square extends RectangleComponent with TapCallbacks {
-  static const speed = 3;
-  static const squareSize = 128.0;
-  static const indicatorSize = 6.0;
-
-  static Paint red = BasicPalette.red.paint();
-  static Paint blue = BasicPalette.blue.paint();
-
-  Square(Vector2 position)
-      : super(
-          position: position,
-          size: Vector2.all(squareSize),
-          anchor: Anchor.center,
-        );
-
-  @override
-  void update(double dt) {
-    super.update(dt);
-    angle += speed * dt;
-    angle %= 2 * math.pi;
-  }
-
-  @override
-  Future<void> onLoad() async {
-    super.onLoad();
-    add(
-      RectangleComponent(
-        size: Vector2.all(indicatorSize),
-        paint: blue,
-      ),
-    );
-    add(
-      RectangleComponent(
-        position: size / 2,
-        size: Vector2.all(indicatorSize),
-        anchor: Anchor.center,
-        paint: red,
-      ),
-    );
-  }
-
-  @override
-  void onTapDown(TapDownEvent event) {
-    removeFromParent();
-    event.handled = true;
-  }
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(GameWidget(game: GlobGame()));
 }
